e1744e878c977209ca408adb34d07f64a1e4122b
Samir Trajano Feitosa <samir.feitosa@virtus.ufcg.edu.br>
2019-10-12 10:07:33 -0300

    add protocol version and its tests

---
+++ sipuadart/lib/sdp/version/protocol_version.dart
@@ -0,0 +1,24 @@
+import 'package:sipuadart/sdp/field.dart';
+
+class ProtocolVersion extends Field {
+
+  static const _v = 118;
+  static const _defaultVersion = 0;
+
+  int _version = _defaultVersion;
+  final int _type = _v;
+
+  ProtocolVersion(this._version);
+
+  ProtocolVersion.standard() : this(_defaultVersion);
+
+  int get type => _type;
+
+  int get version => _version;
+
+  @override
+  String toString() {
+    return "${String.fromCharCode(type)}=$version";
+  }
+
+}
---
+++ sipuadart/test/sdp/protocol_version_test.dart
@@ -0,0 +1,37 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sipuadart/sdp/version/protocol_version.dart';
+
+void main() {
+
+  const _typeV = 118;
+  const _protocol = 'v';
+
+  group("Protocol Version", () {
+
+    test("compare attribute value as decimal", () {
+      expect(_protocol, String.fromCharCode(_typeV));
+    });
+
+    test('build a standard protocol version', () {
+      final protocolVersion = ProtocolVersion.standard();
+      final version = 0;
+      expect(protocolVersion.version, version);
+      expect(protocolVersion.type, _typeV);
+      expect(protocolVersion.toString(), '$_protocol=$version');
+    });
+
+    test('build a protocol version', () {
+      final version = 1;
+      final protocolVersion = ProtocolVersion(version);
+      expect(protocolVersion.version, version);
+      expect(protocolVersion.type, _typeV);
+      expect(protocolVersion.toString(), '$_protocol=$version');
+    });
+
+  });
+
+
+
+
+
+}
